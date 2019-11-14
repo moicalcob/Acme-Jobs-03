396d8105ba58b430ee8109a4ad434bf20babd6b9
María Casasola <Mara Casasola@10.100.154.73>
2019-11-14 18:02:23 +0100

    Task004 finished. You can delete, update and create any announcement now.

--- src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementController.java
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementController.java
@@ -23,11 +23,23 @@ public class AdministratorAnnouncementController extends AbstractController<Admi
 	@Autowired
 	private AdministratorAnnouncementShowService	showService;
 
+	@Autowired
+	private AdministratorAnnouncementCreateService	createService;
+
+	@Autowired
+	private AdministratorAnnouncementUpdateService	updateService;
+
+	@Autowired
+	private AdministratorAnnouncementDeleteService	deleteService;
+
 
 	//Constructors
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
 		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+		super.addBasicCommand(BasicCommand.UPDATE, this.updateService);
+		super.addBasicCommand(BasicCommand.DELETE, this.deleteService);
 	}
 }
